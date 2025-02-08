cab21eb40a87f4249934166561ee831ae9e46217
mgdApp <llsmgdz@gmail.com>
2025-02-07 19:14:17 -0600

    Subiendo el programa.

---
+++ clock1.py
@@ -0,0 +1,14 @@
+import time
+import os
+
+def clear_console():
+    # Limpia la consola según el sistema operativo
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+while True:
+    clear_console()
+    # Obtiene la hora actual en formato HH:MM:SS
+    current_time = time.strftime("%H:%M:%S")
+    print("Reloj Digital (Consola):", current_time)
+    # Espera 1 segundo antes de actualizar
+    time.sleep(1)
